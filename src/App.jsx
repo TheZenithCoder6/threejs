8e052329f4863a062cdd4a0e3469f2dd05f988f3
HarshitGupta <harshitgupta.e2xinfotech@gmail.com>
2026-05-25 16:03:06 +0530

    Fix: removed duplicate import and fixed case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import {BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import './App.css'
-import ComputersCanvas from './components/canvas/Computers'
-import About from './components/About'
-import Experience from './components/Experience'
-import Tech from './components/Tech'
-import Works from './components/Works'
-import Feedbacks from './components/feedbacks'; 
-import Contact from './components/Contact'
-import Feedbacks from './components/feedbacks';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import './App.css';
+import ComputersCanvas from './components/canvas/Computers';
+import About from './components/About';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import Feedbacks from './components/Feedbacks'; 
+import Contact from './components/Contact';
 import StarsCanvas from "./components/canvas/Stars";
 
 function App() {
-
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -35,9 +32,7 @@ function App() {
         </div>
       </div>
     </BrowserRouter>
-      
-  
-  )
+  );
 }
 
-export default App
+export default App;
